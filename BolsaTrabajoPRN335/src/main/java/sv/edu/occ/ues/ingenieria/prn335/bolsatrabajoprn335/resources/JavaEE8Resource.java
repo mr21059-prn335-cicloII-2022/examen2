852e48119a841c3ac0b1a956891fec045c2f938b
mr21059-prn335-cicloII-2022 <100311445+mr21059-prn335-cicloII-2022@users.noreply.github.com>
2022-10-28 17:53:18 -0600

    Add files via upload
    Añadido proyecto de examen 2

---
+++ BolsaTrabajoPRN335/src/main/java/sv/edu/occ/ues/ingenieria/prn335/bolsatrabajoprn335/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package sv.edu.occ.ues.ingenieria.prn335.bolsatrabajoprn335.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
